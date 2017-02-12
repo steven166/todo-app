ea7e678a224c468444a5215e6d0c0c6399f065be
Steven Hermans <steven166@live.nl>
2017-02-12 13:14:59 +0100

    init

---
+++ src/test/java/com/example/todo/TodoControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.todo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Steven Hermans
+ */
+public class TodoControllerTest {
+
+    @Test
+    public void test(){
+        assertEquals(5, 5);
+    }
+
+}
